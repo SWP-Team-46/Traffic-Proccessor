119166bf53ff1032d268eced493d28137d0396b3
inseeee <kalka.vika.perm@gmail.com>
2026-06-30 17:10:56 +0300

    Update app.py

--- error_server/app.py
+++ error_server/app.py
@@ -1,1 +1,17 @@
+from flask import Flask, request
 
+app = Flask(__name__)
+
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def error_page(path):
+    return f"""
+    <h1>Access denied</h1>
+    <p>Your IP from error server view: {request.remote_addr}</p>
+    <p>Path: /{path}</p>
+    """, 403
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
